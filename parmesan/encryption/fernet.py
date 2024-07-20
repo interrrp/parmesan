04e366b2333a4142b6769393dcdbbddd7e58093d
interrrp <int@addictedto.coffee>
2024-07-20 22:08:54 +0800

    refactor: `FernetEncryptor`

--- parmesan/encryption/fernet.py
+++ parmesan/encryption/fernet.py
@@ -1,9 +1,9 @@
 import base64
 import os
+from functools import lru_cache
 from typing import override
 
 from cryptography.fernet import Fernet, InvalidToken
-from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
@@ -14,12 +14,17 @@ from parmesan.encryption.errors import IncorrectMasterPasswordError
 class FernetEncryptor(Encryptor):
     """Implementation of `Encryptor` using Fernet symmetric encryption."""
 
-    def derive_key(self, master_password: str, salt: bytes) -> bytes:
+    SALT_SIZE = 16
+
+    @staticmethod
+    @lru_cache
+    def derive_key(master_password: str, salt: bytes, iterations: int = 100_000) -> bytes:
         """Derive a key from the master password and salt using PBKDF2.
 
         Args:
             master_password: The master password.
             salt: The salt for key derivation.
+            iterations: The number of iterations for key derivation. Defaults to 100,000.
 
         Returns:
             The derived key as bytes.
@@ -29,25 +34,28 @@ class FernetEncryptor(Encryptor):
             algorithm=hashes.SHA256(),
             length=32,
             salt=salt,
-            iterations=100000,
-            backend=default_backend(),
+            iterations=iterations,
         )
         return base64.urlsafe_b64encode(kdf.derive(master_password.encode()))
 
     @override
     def encrypt(self, plaintext: str, master_password: str) -> str:
-        salt = os.urandom(16)
-        key = self.derive_key(master_password, salt)
+        salt = os.urandom(self.SALT_SIZE)
+        key = FernetEncryptor.derive_key(master_password, salt)
+
         cipher_suite = Fernet(key)
         encrypted_text = cipher_suite.encrypt(plaintext.encode())
+
         return base64.urlsafe_b64encode(salt + encrypted_text).decode()
 
     @override
     def decrypt(self, ciphertext: str, master_password: str) -> str:
         decoded_data = base64.urlsafe_b64decode(ciphertext.encode())
-        salt = decoded_data[:16]
-        encrypted_text = decoded_data[16:]
-        key = self.derive_key(master_password, salt)
+
+        salt = decoded_data[: FernetEncryptor.SALT_SIZE]
+        encrypted_text = decoded_data[FernetEncryptor.SALT_SIZE :]
+        key = FernetEncryptor.derive_key(master_password, salt)
+
         cipher_suite = Fernet(key)
         try:
             return cipher_suite.decrypt(encrypted_text).decode()
